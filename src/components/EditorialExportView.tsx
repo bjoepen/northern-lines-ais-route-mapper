5c811a2efc0593becddd1717d76cbd2549f0df4c
bjoepen <bernd.joepen@t-online.de>
2026-09-13 10:53:19 +0200

    build 005B-D export editor

--- src/components/EditorialExportView.tsx
+++ src/components/EditorialExportView.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { VoyageData } from '../types';
-import { GapDisplayAdjustment } from '../project/nlroute';
+import { EditorialContent, GapDisplayAdjustment } from '../project/nlroute';
 import { EditorialMapPreview } from './EditorialMapPreview';
-
-type Props = { voyage: VoyageData; gapAdjustments: GapDisplayAdjustment[] };
-
-export const EditorialExportView: React.FC<Props> = ({ voyage, gapAdjustments }) => (
-  <div className="w-full h-full overflow-y-auto bg-[#f4f1e9] p-6">
-    <div className="max-w-6xl mx-auto">
-      <EditorialMapPreview voyage={voyage} gapAdjustments={gapAdjustments}/>
-    </div>
-  </div>
-);
+import { EditorialContentEditor } from './EditorialContentEditor';
+type Props={voyage:VoyageData;gapAdjustments:GapDisplayAdjustment[];editorial:EditorialContent;onEditorialChange:(next:EditorialContent)=>void};
+export const EditorialExportView:React.FC<Props>=({voyage,gapAdjustments,editorial,onEditorialChange})=><div className="w-full h-full overflow-y-auto bg-[#f4f1e9] p-6"><div className="max-w-7xl mx-auto flex gap-5 items-start"><div className="w-[300px] shrink-0"><EditorialContentEditor value={editorial} onChange={onEditorialChange}/></div><div className="min-w-0 flex-1"><EditorialMapPreview voyage={voyage} gapAdjustments={gapAdjustments} editorial={editorial}/></div></div></div>;
