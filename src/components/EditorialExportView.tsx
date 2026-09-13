ae7ed58612b15163fc8e46adfb2c353feac796e7
bjoepen <bernd.joepen@t-online.de>
2026-09-13 13:19:25 +0200

    build 005B-E: wire editorial output controls

--- src/components/EditorialExportView.tsx
+++ src/components/EditorialExportView.tsx
@@ -3,5 +3,7 @@ import { VoyageData } from '../types';
 import { EditorialContent, GapDisplayAdjustment } from '../project/nlroute';
 import { EditorialMapPreview } from './EditorialMapPreview';
 import { EditorialContentEditor } from './EditorialContentEditor';
+import { EditorialOutputControls } from './EditorialOutputControls';
+
 type Props={voyage:VoyageData;gapAdjustments:GapDisplayAdjustment[];editorial:EditorialContent;onEditorialChange:(next:EditorialContent)=>void};
-export const EditorialExportView:React.FC<Props>=({voyage,gapAdjustments,editorial,onEditorialChange})=><div className="w-full h-full overflow-y-auto bg-[#f4f1e9] p-6"><div className="max-w-7xl mx-auto flex gap-5 items-start"><div className="w-[300px] shrink-0"><EditorialContentEditor value={editorial} onChange={onEditorialChange}/></div><div className="min-w-0 flex-1"><EditorialMapPreview voyage={voyage} gapAdjustments={gapAdjustments} editorial={editorial}/></div></div></div>;
+export const EditorialExportView:React.FC<Props>=({voyage,gapAdjustments,editorial,onEditorialChange})=><div className="w-full h-full overflow-y-auto bg-[#f4f1e9] p-6"><div className="max-w-7xl mx-auto flex gap-5 items-start"><div className="w-[300px] shrink-0 space-y-4"><EditorialContentEditor value={editorial} onChange={onEditorialChange}/><EditorialOutputControls voyage={voyage}/></div><div className="min-w-0 flex-1"><EditorialMapPreview voyage={voyage} gapAdjustments={gapAdjustments} editorial={editorial}/></div></div></div>;
